0f4c437ecc20ae0cd91da5a5145528f56c010c9e
Kaitlin Regan <karegan@gmail.com>
2021-04-08 12:11:31 -0400

    index.js sends data to backend

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
 
-
-document.addEventListener("DOMContentLoaded", () => {
     //grab all the necessary DOM elements
     
   
@@ -10,40 +8,71 @@ document.addEventListener("DOMContentLoaded", () => {
   
     //ul where new stores will live on the DOM
     const newStoreUl = document.getElementById("stores");
+
+const createNewStore = event => {
+    event.preventDefault();
+    //stop form from trying to submit
+
+    const newStoreName = document.getElementById("new-store-name");
+    const newStore = document.createElement("li");
+    newStore.innerText = newStoreName.value;
+
+    appendNewStore(newStore);
+    event.target.reset();
+};
+
+const appendNewStore = store => {
+    document.getElementById("stores").appendChild(store);
+}
+
+function fetchStores(){
+    fetch('http://localhost:3000/stores')
+    .then(res => res.json())
+    .then(addStores)
+}
+
+function addStores(response){
+    response.forEach( store => {
+      //   createNewStore(store)
+      const newStoreUl = document.getElementById("stores");
+      newStoreUl.innerHTML += `<li> ${store.name} </li>`
+    })
+}
+
+function handleFormSubmit(e){
+    e.preventDefault()
+
+    let newStoreObj = {
+        name: newStoreName.value
+    }
+
+    let configObj = {
+        method: 'POST',
+        headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json"
+        },
+        body: JSON.stringify(newStoreObj)
+    }
+    fetch('http://localhost:3000/stores', configObj)
+    .then(res => res.json())
+    .then(data => {
+        debugger
+    })
+}
+
+
+
+
+
+
+document.addEventListener("DOMContentLoaded", () => {
+
   
     //attach event listeners
     fetchStores()
-    newStoreForm.addEventListener("submit", createNewStore)
-  
+    newStoreForm.addEventListener("submit", handleFormSubmit)
   
   });
   
-  const createNewStore = event => {
-      event.preventDefault();
-      //stop form from trying to submit
   
-      const newStoreName = document.getElementById("new-store-name");
-      const newStore = document.createElement("li");
-      newStore.innerText = newStoreName.value;
-  
-      appendNewStore(newStore);
-      event.target.reset();
-  };
-  
-  const appendNewStore = store => {
-      document.getElementById("stores").appendChild(store);
-  }
-
-  function fetchStores(){
-      fetch('http://localhost:3000/stores')
-      .then(res => res.json())
-      .then(addStores)
-  }
-
-  function addStores(response){
-      response.forEach( store => {
-        //   createNewStore(store)
-        const newStoreUl = document.getElementById("stores");
-        newStoreUl.innerHTML += `<li> ${store.name} </li>`
-      })
-  }
